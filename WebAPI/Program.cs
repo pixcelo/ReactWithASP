ee72c875b0d4f0294b31bce5e0c82d2cc814cd4d
pixcelo <bodomu@gmail.com>
2024-05-29 11:45:41 +0900

    update : CORS有効化

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -7,6 +7,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CORS‚Ì—LŒø‰»
+builder.Services.AddCors(opsions =>
+{
+    opsions.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173");
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +27,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
